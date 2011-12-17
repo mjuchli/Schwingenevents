9e9128d8be15b7d4c97b0b8cbdafa7944ac47a6d
marc <marc@likeme.ch>
2011-12-17 01:39:15 +0100

    plugin description
    athleten file as a refference

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php 
-echo "test";
+/*
+Plugin Name: Events
+Plugin URI: http://www.impres-sign.com/
+Description: Events-Register mit Download von Rangliste. In unserem Falle wird dies fuer den schweizer Schwingsport gebraucht, um alle Schwingfeste und Events zu erfassen.
+Version: 1.0
+Author: Marc Juchli
+Author URI: http://www.impres-sign.com/
+License: GPL
+Stable Tag: 1.0
+*/
 ?>
+
